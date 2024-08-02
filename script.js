9f4b98a36897f3320525b7e388aaea7b9a130e10
Zahra Malikzada <zara@Zahras-MacBook-Air.local>
2024-08-03 00:04:16 +0400

    table finish

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let name_input = document.getElementById("name_input");
 let lastname_input = document.getElementById("lastname_input");
 let age_input = document.getElementById("age_input");
 let email_input = document.getElementById("email_input");
-let table_element = document.getElementsByTagName("table");
+let table_element = document.querySelector(".table");
+let table_body = document.querySelector(".tbody");
 
 // Main array
 let users = [];
@@ -27,14 +28,25 @@ function getPersonData() {
   email_input.value = "";
 }
 
-function showPerson(){  
-  const td_element = document.createElement("td");
-  td_element.innerText = 'Salam'
+function showPerson(){ 
+  table_body.innerHTML = "";
+  table_element.style.display = "table";
+  for(let i=0; i<users.length; i++){
+    let tr = document.createElement("tr");
+    let td_name = document.createElement("td");
+    let td_lastname = document.createElement("td");
+    let td_age = document.createElement("td");
+    let td_email = document.createElement("td");
+    let td_date = document.createElement("td");
 
-  const tr_element = document.createElement("tr");
+    td_name.innerText = users[i].name;
+    td_lastname.innerText = users[i].lastname;
+    td_age.innerText = users[i].age;
+    td_email.innerText = users[i].email;
+    td_date.innerText = users[i].date
 
-  const tbody = document.createElement("tbody");
-
-  tr_element.append(td_element);
-  table_element.appendChild(tr_element);
+    tr.append(td_name, td_lastname, td_age, td_email, td_date);
+    table_body.append(tr)
+    table_element.append(table_body);
+  }
 }
